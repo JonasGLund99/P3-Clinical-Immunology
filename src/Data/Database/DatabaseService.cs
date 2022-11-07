b9f770cbd2107c7f7884a2af76f63556a1a1feaa
Anders Toft <atoft20@student.aau.dk>
2022-11-07 14:02:13 +0100

    Database setup

---
+++ src/Data/Database/DatabaseService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Azure.Cosmos;
+namespace src.Data;
+
+public class DatabaseService 
+{
+    private static readonly DatabaseService instance = new DatabaseService();
+
+    private CosmosClient client;
+    public Database? Database;
+    private DatabaseService()
+    {
+        client = new CosmosClient("https://p3-database.documents.azure.com:443/", "hB3Q6QxvgjERggOZC3vjRuRjzYjbpBJMXM6vKK2bboF3D4fBhDMmfsI0sHpk5qFIdzNSozPuGSoyF3cOzoeIjg==");
+        SetupDatabase();
+    }
+    public static DatabaseService Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    
+    public async void SetupDatabase()
+    {
+        Database = await client.CreateDatabaseIfNotExistsAsync("ClinicalImmunology",1000);
+        await Database.CreateContainerIfNotExistsAsync("Experiment", "/id");
+        await Database.CreateContainerIfNotExistsAsync("ClinicalTest", "/id");
+        await Database.CreateContainerIfNotExistsAsync("Slide", "/id");
+        await Database.CreateContainerIfNotExistsAsync("Block", "/id");
+    }
+
+}
