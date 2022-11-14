7763d1fa866e9f53491e82639392a4a82330da28
Asbjørn Christensen <69584571+AsbjoernJC@users.noreply.github.com>
2022-11-14 10:44:41 +0100

    Update Mocker.cs

--- src/Data/MockedData/Mocker.cs
+++ src/Data/MockedData/Mocker.cs
@@ -48,28 +48,3 @@ static class Mocker
         }
     }
 }
-
-
-class Exp : BaseModel<Exp> {
-    public Exp(string id, string experimentNumber, string title, string author, string description) : base(id)
-    {
-        ExperimentNumber = experimentNumber;
-        Title = title;
-        Author = author;
-        Description = description;
-    }
-    public string ExperimentNumber { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string Description { get; set; }
-}
-
-class CT : BaseModel<CT> {
-    public CT(string id, string title, List<string> experimentIds) : base(id)
-    {
-        Title = title;
-        ExperimentIds = experimentIds;
-    }
-    public string Title { get; set; }
-    public List<string> ExperimentIds { get; set; }
-}
