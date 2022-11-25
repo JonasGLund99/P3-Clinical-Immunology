5143298feac8f5cda0c35f8237af8a676a359dc6
AsbjoernJC <asbjoernjc@gmail.com>
2022-11-25 18:44:32 +0100

    added some test

---
+++ src.Tests/CollectionBehaviorOverride.cs
@@ -0,0 +1,5 @@
+﻿using Xunit;
+
+// This is important as the test of calculateclinicaltestresult must
+// happen in a specfic order. Therefore parallelization is disabled?
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
---
+++ src.Tests/Slide.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Slide
+{
+    private readonly Slide _sut;
+    public Slide()
+    {
+        _sut = new Slide();
+    }
+}
