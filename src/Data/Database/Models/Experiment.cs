9735772f17700c0e49401e2b7241ac9f8e9e2bf9
Anders Toft <atoft20@student.aau.dk>
2022-11-25 16:16:53 +0100

    new sql string for search in clinical test

--- src/Data/Database/Models/Experiment.cs
+++ src/Data/Database/Models/Experiment.cs
@@ -35,12 +35,19 @@ public class Experiment : BaseModel<Experiment>
         {
             throw new NullReferenceException("No database");
         }
-        string queryString = @"SELECT DISTINCT VALUE CT FROM ClinicalTest CT
-                            JOIN s IN CT.Slides
-                            WHERE (CONTAINS(CT.Title, @searchParameter, true)
-                            OR CONTAINS(s.Barcode, @searchParameter, true))
-                            AND ARRAY_CONTAINS(CT.ExperimentIds, @expId)
-                            ORDER BY CT.EditedAt DESC";
+
+        string queryString = @"SELECT VALUE CT
+                                FROM ClinicalTest CT
+                                WHERE (
+                                    EXISTS (
+                                        SELECT VALUE s
+                                        FROM s IN CT.Slides
+                                        WHERE CONTAINS(s.Barcode, @searchParameter, true)
+                                        )
+                                    OR CONTAINS(CT.Title, @searchParameter, true)
+                                    )
+                                    AND ARRAY_CONTAINS(CT.ExperimentIds, @expId)
+                                ORDER BY CT.EditedAt DESC ";
 
         FeedIterator<ClinicalTest> feed = DatabaseService.Instance.Database.GetContainer("ClinicalTest")
                                         .GetItemQueryIterator<ClinicalTest>(
