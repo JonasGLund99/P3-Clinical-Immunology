28a24cb060a9189f71a29459bd626c8164353376
unknown <asbjoernjc@gmail.com>
2022-12-05 20:54:08 +0100

    working on removeblank

---
+++ src.Tests/ClinicalTest/RemoveBlankBlock.cs
@@ -0,0 +1,166 @@
+using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+using src.Data;
+using System.Collections;
+using Xunit;
+
+namespace src.Tests;
+
+//https://youtu.be/2Wp8en1I9oQ?t=1152 Se delen om Reusing Instances
+public class RemoveBlankBlocksTest
+{
+    private readonly ClinicalTest _sut;
+
+    public RemoveBlankBlocksTest()
+    {
+        _sut = new ClinicalTest();
+    }
+
+
+    [Theory]
+    [ClassData(typeof(GetBlankBlocksTestData))]
+    public async void RemoveBlankBlocks(List<Block> expected, List<Block> mockedBlocks, params ClinicalTest[] clinicalTests)
+    {
+        List<Block> blocks = new();
+
+        await DatabaseService.Instance.SetupDatabase();
+        if (DatabaseService.Instance.Database == null) throw new Exception("Database did not complete setup for GetBlankBlocks test");
+
+        foreach (ClinicalTest c in clinicalTests)
+        {
+            foreach (Block block in mockedBlocks)
+            {
+                await c.AddBlankBlock(block);
+            }
+            blocks.AddRange(await c.GetBlankBlocks());
+        }
+
+        var serializedExpected = JsonConvert.SerializeObject(expected);
+        var serializedActual = JsonConvert.SerializeObject(blocks);
+        Assert.Equal(serializedExpected, serializedActual);
+
+        List<Block> blocks2 = new();
+        List<Block> expected2 = new List<Block>() {
+                new Block { SlideIndex = 0, BlockIndex = 1, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 2, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 3, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 4, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 5, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 6, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 7, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 8, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 9, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 10, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 11, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 12, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 13, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 14, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 15, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 16, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 17, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 18, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 19, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 20, Type = Block.BlockType.Blank }, };
+
+        foreach (ClinicalTest c in clinicalTests)
+        {
+            await c.RemoveBlankBlock(mockedBlocks[0]);
+            blocks2 = await c.GetBlankBlocks();
+        }
+
+        var serializedExpected2 = JsonConvert.SerializeObject(expected2);
+        var serializedActual2 = JsonConvert.SerializeObject(blocks2);
+
+        Assert.Equal(serializedExpected2, serializedActual2);
+    }
+
+    //[Fact]
+    //public async void RemoveBlankBlockNothingRemoved()
+    //{
+    //    ClinicalTest c2 = new ClinicalTest();
+    //    c2.BlankBlockIds = new List<string>() {
+    //            "Vi ses til julefrokost"
+    //        };
+
+    //    await DatabaseService.Instance.SetupDatabase();
+    //    if (DatabaseService.Instance.Database == null) throw new Exception("Database did not complete setup for this test");
+
+    //    await Assert.ThrowsAnyAsync<NullReferenceException>(c2.GetBlankBlocks);
+
+    //}
+
+
+
+    public class GetBlankBlocksTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            ClinicalTest c1 = new ClinicalTest();
+            MockedNormalBlocks mb = new MockedNormalBlocks();
+
+
+            yield return new object[] { new List<Block>
+            {
+                new Block { SlideIndex = 0, BlockIndex = 0, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 1, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 2, Type = Block.BlockType.Blank },
+                new Block { SlideIndex = 0, BlockIndex = 3, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 4, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 5, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 6, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 7, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 8, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 9, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 10, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 11, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 12, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 13, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 14, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 15, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 16, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 17, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 18, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 19, Type = Block.BlockType.Blank},
+                new Block { SlideIndex = 0, BlockIndex = 20, Type = Block.BlockType.Blank},
+            }, mb.Blocks, c1 };
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class MockedNormalBlocks
+        {
+            private int numSlides = 1;
+            public List<Block> Blocks { get; set; }
+
+            public MockedNormalBlocks()
+            {
+                Blocks = generateBlocks();
+            }
+
+            public List<Block> generateBlocks()
+            {
+                Blocks = new List<Block>();
+
+                for (int j = 0; j < numSlides; j++)
+                {
+                    for (int i = 0; i < 21; i++)
+                    {
+                        Block block = new Block();
+                        block.SlideIndex = j;
+                        block.BlockIndex = i;
+                        block.Type = Block.BlockType.Blank;
+                        Blocks.Add(block);
+                    }
+                }
+
+
+                return Blocks;
+            }
+        }
+    }
+
+}
